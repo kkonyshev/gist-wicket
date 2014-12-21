e86442afa4caeb34e46a072bd5549f4301ee768b
Konstantin k <hs@ngs.ru>
2014-12-22 01:37:16 +0700

    integration test assert; converting to multi-module project

--- web-app/src/test/java/kkonyshev/web_app/ApiIntegrationTest.java
+++ web-app/src/test/java/kkonyshev/web_app/ApiIntegrationTest.java
@@ -3,12 +3,17 @@ package kkonyshev.web_app;
 import java.io.IOException;
 import java.net.URISyntaxException;
 
+import kkonyshev.web.api.HelloRes;
+
 import org.apache.http.HttpException;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.junit.Assert;
 import org.junit.Test;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ApiIntegrationTest {
 
@@ -17,10 +22,11 @@ public class ApiIntegrationTest {
 		String name = "user";
 		HttpUriRequest request = new HttpGet( "http://localhost:8080/api/hello/" + name  );
 		
-		// Whenas
+		// When
 		HttpResponse httpResponse = new DefaultHttpClient().execute(request);
 		
-		// Then
-		System.out.println(httpResponse);
+		// Then\
+		HelloRes response = new ObjectMapper().readValue(httpResponse.getEntity().getContent(), HelloRes.class);
+		Assert.assertEquals(name, response.getName());
 	}
 }
